f14f6d7aba74f1e1fc3e7ec01b54d6de87c5b12b
Hassan Sajjad <sajjadhassa389@gmail.com>
2023-09-06 01:39:53 +0500

    Feature/First_Commit_Initial_Changes

---
+++ src/pages/Admission.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar} from '@syncfusion/ej2-react-grids';
+import { employeesData , admissionGrid } from '../data/dummy';
+import { Header } from '../components';
+import axios from "axios";
+const Admission = () => {
+  const [admission , setAdmission] = useState("")
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/admission/get"
+      );
+      const data = response.data;
+      setAdmission(data)
+      console.log("DATAAAAA", admission);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Admissions' />
+      <GridComponent dataSource={admission} allowPaging allowSorting toolbar={['Search']} width="auto">
+        <ColumnsDirective>
+        {admissionGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ) )}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  )
+}
+
+export default Admission
